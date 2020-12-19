8afb1f9ad1f50d83e7d5ab41ea69a9f9e3dfb97e
Seven-Silent-Stars <73199996+Seven-Silent-Stars@users.noreply.github.com>
2020-12-19 11:16:38 -0400

    Add files via upload

---
+++ curioso.js
@@ -0,0 +1,27 @@
+let recipiente = "la remil puta que te parió dale siempre ok";
+let recipiente2 = "cara de puto te dije que esto nonestaba terminado";
+let recipiente3 = "capricho de la deformidad humana es un buen insulto para vos";
+alert(recipiente)
+alert(recipiente2)
+alert(recipiente3)
+
+let numero = "5";
+let numero2 = "mentarios";
+alert(numero + numero2)
+
+let nombre = prompt("decime un buen insulto")
+
+if (nombre == "no"){
+alert("eres un desperdicio de semen suicidate")
+}
+
+else if(nombre == "y si no quiero que"){
+alert("te parto tu madre")
+}
+
+else if(nombre == "no se"){
+alert("te falta odio")
+}
+else{
+alert("Por cierto hola, " + nombre)
+}
