a062475c226fd5e88a3864cc5a048c063266d78e
Seven-Silent-Stars <73199996+Seven-Silent-Stars@users.noreply.github.com>
2021-01-15 19:03:41 -0400

    Update curioso.js

--- curioso.js
+++ curioso.js
@@ -1,5 +1,5 @@
 let recipiente = "la remil puta que te parió dale siempre ok";
-let recipiente2 = "cara de puto te dije que esto nonestaba terminado";
+let recipiente2 = "cara de puto te dije que esto no estaba terminado";
 let recipiente3 = "capricho de la deformidad humana es un buen insulto para vos";
 alert(recipiente)
 alert(recipiente2)
@@ -12,16 +12,25 @@ alert(numero + numero2)
 let nombre = prompt("decime un buen insulto")
 
 if (nombre == "no"){
-alert("eres un desperdicio de semen suicidate")
+alert("eres un desperdicio de semen, suicidate")
 }
 
-else if(nombre == "y si no quiero que"){
+else if(nombre == "y si no quiero que?"){
 alert("te parto tu madre")
 }
 
 else if(nombre == "no se"){
 alert("te falta odio")
 }
+
+else if(nombre == "ok"){
+alert("ok ¿qué? ¿te rrompo las pelotas puto?")}
+
+else if(nombre == "si"){
+alert("Si qué? te parto el orto?")}
+
+
+
 else{
 alert("Por cierto hola, " + nombre)
 }
